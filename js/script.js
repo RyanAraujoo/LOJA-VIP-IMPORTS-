69b91f112d18c9553a69db69fd5b0931823aadc7
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-01-15 19:31:56 -0300

    correção de bugs no header e de garranchos via css

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@ function showNav() {
     var buttonAtive = document.getElementById("navbar-responsive")
         
        if (buttonAtive.style.display == 'none' || buttonAtive.style.display == '') {
-            buttonAtive.style.display = 'block'
-            buttonAtive.className = "add-transit-Click"
+        buttonAtive.style.display = 'block'
+            
        } else {
-            buttonAtive.style.display = 'none'
+        buttonAtive.style.display = 'none'
     }
 }
 function transitClick() {
-    var btn =  document.getElementById("header-flex-form")
-     btn.className = "add-transit-Click"
+    document.getElementById("header-flex-form").style.opacity = 3
 }
